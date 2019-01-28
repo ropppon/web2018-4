422e6e2e01da596d7133e406ab32ac3270f9c203
zm16641 <zm16641@josai.ac.jp>
2019-01-27 20:51:30 -0500

    message

---
+++ yu4.js
@@ -0,0 +1,7 @@
+alert('Hello,Roppongi!');
+function higasinihon() {
+  document.getElementById('myImage').src='./image/higasinihon.jpg';
+}
+function himawari() {
+  document.getElementById('myImage').src='./image/himawari.jpg';
+}
